237788aba1be30cc43bd90b4655cf903914f89e8
NguyenLanAnh2299 <43206459+NguyenLanAnh2299@users.noreply.github.com>
2020-11-23 18:11:59 +0700

    find product bay name, category, price

---
+++ LinearSearch/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinearSearch
+{
+    class Product
+    {
+            public string name { get; set; }
+            public double price { get; set; }
+            public int quantity { get; set; }
+            public int categoryId { get; set; }
+            public virtual Category Category { get; set; }
+    }
+}
---
+++ LinearSearch/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinearSearch
+{
+    class Program
+    {
+        //    public static void Main(string[] args)
+        //    {
+        //} 
+       
+        //List product by name
+        public static Product FindProduct(List<Product> products, string name)
+        {
+            Product product = new Product();
+            for (int i = 0; i < products.Count(); i++)
+            {
+                if (products[i].name == name)
+                {
+                    product = products[i];
+                }
+            }
+            return product;
+        }
+        //list product by category
+        public static List<Product> FindProductByCategory(List<Product> products, int Id)
+        {
+            List<Product> results = new List<Product>();
+
+            for (int i = 0; i < products.Count(); i++)
+            {
+                if (products[i].categoryId == Id)
+                {
+                    results.Add(products[i]);
+                }
+            }
+            return results;
+        }
+        //list product by price
+        public static List<Product> FindProductByPrice(List<Product> products, double price)
+        {
+            List<Product> results = new List<Product>();
+            for (int i = 0; i < products.Count(); i++)
+            {
+                if(products[i].price <= price)
+                {
+                    results.Add(products[i]);
+                }
+            }
+            return results;
+        }
+
+    }
+}
